7ec1106574a35869919f9e0c20be1f165e3e4a7d
Lara Antón Güemes <lantog00@estudiantes.unileon.es>
2026-04-28 18:46:38 +0200

    creacion main

---
+++ main.py
@@ -0,0 +1,18 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
+from src.modelo.vista.Login import MiVentana
+from src.modelo.Logica import Logica
+from src.controlador.ControladorPrincipal import ControladorPrincipal
+
+import os.path
+os.path.dirname(os.path.abspath(__file__))
+if __name__ == "__main__":
+    app = QApplication([])
+    ventana = MiVentana()
+    modelo=Logica()
+    controlador= ControladorPrincipal(ventana, modelo)
+
+    ventana.controlador=controlador 
+    controlador.ventanaIniciarSesion()
+
+    app.exec_()
