0709250465a12a43fad04e2bc3fbf343eed88e12
Nuria Garcia Arnaiz <ngarca08@estudiantes.unileon.es>
2026-05-04 01:43:16 +0200

    Add Logica class with ejemploSelect method

--- src/modelo/Logica.py
+++ src/modelo/Logica.py
@@ -1,1 +1,9 @@
+from src.modelo.dao.UserDAO import UserDAO
 
+class Logica():
+  def ejemploSelect(self):
+    userDAO = UserDAO()
+    usuarios = userDAO.select()
+
+    for user in usuarios:
+      print(user.__nombre)
