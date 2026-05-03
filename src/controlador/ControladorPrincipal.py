5c9087ca7026c4f1381361364bfc662b92ee82a6
Nuria Garcia Arnaiz <ngarca08@estudiantes.unileon.es>
2026-05-04 01:37:43 +0200

    Implement ControladorPrincipal for login management
    Added ControladorPrincipal class to manage login functionality.

--- src/controlador/ControladorPrincipal.py
+++ src/controlador/ControladorPrincipal.py
@@ -1,1 +1,20 @@
+from src.modelo.vo.LoginVO import LoginVO
+from src.modelo.Logica import Logica
 
+class ControladorPrincipal:
+    def __init__(self, ref_vista, ref_modelo):
+      self.__vista = ref_vista
+      self._modelo = ref_modelo
+
+    def abrirIniciarSesion(self):
+      self.__vista.show()
+
+    def comprobarLogin(self, nombre, passw):
+      LoginVO = LoginVO(nombre, passw)
+      resultado = self.__modelo.hacerLogin(LoginVO)
+
+      if resultado == None:
+        self.__vista.lanzaraviso()
+
+      else:
+        self.__vista.close()
