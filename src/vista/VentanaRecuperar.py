f2ed188c67e25b3ee87a9e6e59fac25b9f5fc580
Nuria Garcia Arnaiz <ngarca08@estudiantes.unileon.es>
2026-05-04 12:15:25 +0200

    Add VentanaRecuperar class for password recovery

--- src/vista/VentanaRecuperar.py
+++ src/vista/VentanaRecuperar.py
@@ -1,1 +1,14 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtCore import Qt
+from PyQt5 import uic
+from src.modelo.Logica_login import BussinessObject
 
+Form, Window = uic.loadUiType("./src/vista/ui/vistaRecuperar.ui")
+
+class VentanaRecuperar(QMainWindow, Form):
+  def __init__(self):
+    super().__init__()
+    self.setupUi(self)
+    self.logica = BusinessObject()
+
+    self.botonRecuperar.clicked.connect(self.on_actualizar)
