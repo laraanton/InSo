b4ce5205944ede2de6b95040bacc6dfcf11bc1b9
Daniela Pino <dpinoc00@estudiantes.unileon.es>
2026-05-16 13:23:50 +0200

    Swap focus behavior for input fields on return press

--- src/vista/VentanaRecuperar.py
+++ src/vista/VentanaRecuperar.py
@@ -12,9 +12,8 @@ class VentanaRecuperar(QMainWindow, Form):
     self.logica = BussinessObject()
 
     self.botonRecuperar.clicked.connect(self.on_actualizar)
-    self.in_email.returnPressed.connect(lambda: self.in_nueva_contrasena.setFocus())
-
-    self.in_dni.returnPressed.connect(lambda: self.in_dni.setFocus())
+    self.in_email.returnPressed.connect(lambda: self.in_dni.setFocus())              
+    self.in_dni.returnPressed.connect(lambda: self.in_nueva_contrasena.setFocus())  
     self.in_nueva_contrasena.returnPressed.connect(lambda: self.in_confirmar.setFocus())
     self.in_confirmar.returnPressed.connect(lambda: self.botonRecuperar.setFocus())
 
