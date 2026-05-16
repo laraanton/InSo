6af2419390e65ae80ff23f056cdaf02290050ca1
Nuria Garcia Arnaiz <ngarca08@estudiantes.unileon.es>
2026-05-16 21:42:16 +0200

    Update VentanaCliente.py

--- src/vista/VentanaCliente.py
+++ src/vista/VentanaCliente.py
@@ -9,10 +9,14 @@ class VentanaCliente(QMainWindow, Form):
         super().__init__()
         self.setupUi(self)
         self.user = user
+        self.menuCuenta.hide()
         self._connect_signals()
 
     def _connect_signals(self):
         self.btnCuenta.clicked.connect(self._abrir_cuenta)
+        self.btnAjustes.clicked.connect(self._ir_ajustes)
+        self.btnMisViajes.clicked.connect(self._ir_mis_viajes)
+        self.btnCerrarSesion.clicked.connect(self._cerrar_sesion)
         # Buscador
         self.btnBuscar.clicked.connect(self._buscar_paquetes)
         # Tarjetas destacadas
@@ -21,9 +25,21 @@ class VentanaCliente(QMainWindow, Form):
         self.card3_btn.clicked.connect(lambda: self._ver_paquete(3))
 
     def _abrir_cuenta(self):
-        """Abre el panel / ventana de la cuenta del usuario."""
-        QMessageBox.information(self, "Mi cuenta", "Aquí irá la vista de cuenta del usuario.")
+        if self.menuCuenta.isVisible():
+            self.menuCuenta.hide()
+        else:
+            self.menuCuenta.show()
+            self.menuCuenta.raise_()
 
+    def _ir_ajustes(self):
+        self.menuCuenta.hide()
+        # TODO: abrir ventana de ajustes
+        QMessageBox.information(self, "Ajustes", "Aquí irá la vista de ajustes.")
+
+    def _ir_mis_viajes(self):
+        self.menuCuenta.hide()
+        QMessageBox.information(self, "Mis viajes", "Aquí irán tus viajes.")
+    
     def _buscar_paquetes(self):
         """Recoge los filtros del buscador y lanza la búsqueda."""
         destino      = self.in_destino.text().strip()
@@ -58,3 +74,16 @@ class VentanaCliente(QMainWindow, Form):
             "Ver paquete",
             f"Abriendo detalle del paquete {numero}…"
         )
+
+    def _cerrar_sesion(self):
+        resp = QMessageBox.question(
+            self, "Cerrar sesion",
+            "Deseas cerrar la sesion actual?",
+            QMessageBox.Yes | QMessageBox.No,
+            QMessageBox.No,
+        )
+        if resp == QMessageBox.Yes:
+            self.close()
+            from src.vista.Login import MiVentana
+            self.login = MiVentana()
+            self.login.show()
