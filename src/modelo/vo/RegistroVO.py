b7628feb0dab091ff05aacda0f3a7f11866d5f39
Daniela Pino <dpinoc00@estudiantes.unileon.es>
2026-05-29 14:23:14 +0200

    Refactor RegistroVO to use public attributes

--- src/modelo/vo/RegistroVO.py
+++ src/modelo/vo/RegistroVO.py
@@ -1,45 +1,12 @@
-
 class RegistroVO:
-    def __init__(self, dni_nie, nombre_completo, email, 
-                 telefono, password_hash, tipo_usuario='Cliente', preferencia = 'General', preferencia_accesibilidad='Ninguna'):
-        self.__dni_nie         = dni_nie
-        self.__nombre_completo = nombre_completo
-        self.__email           = email
-        self.__telefono        = telefono
-        self.__password_hash   = password_hash
-        self.__tipo_usuario    = tipo_usuario  # por defecto Cliente
-        self.__preferencia = preferencia
-        self.__preferencia_accesibilidad = preferencia_accesibilidad
-
-    @property
-    def dni_nie(self):
-        return self.__dni_nie
-
-    @property
-    def nombre_completo(self):
-        return self.__nombre_completo
-
-    @property
-    def email(self):
-        return self.__email
-
-    @property
-    def telefono(self):
-        return self.__telefono
-
-    @property
-    def password_hash(self):
-        return self.__password_hash
-
-    @property
-    def tipo_usuario(self):
-        return self.__tipo_usuario
-
-    @property
-    def preferencia(self):
-        return self.__preferencia
-        
-    @property
-    def preferencia_accesibilidad(self):
-        return self.__preferencia_accesibilidad
-
+    def __init__(self, dni_nie, nombre_completo, email, telefono, password_hash,
+                 tipo_usuario="Cliente", preferencia="General",
+                 preferencia_accesibilidad="Ninguna"):
+        self.dni_nie = dni_nie
+        self.nombre_completo = nombre_completo
+        self.email = email
+        self.telefono = telefono
+        self.password_hash = password_hash
+        self.tipo_usuario = tipo_usuario
+        self.preferencia = preferencia
+        self.preferencia_accesibilidad = preferencia_accesibilidad
