8138bfe3a055c264976012d79be152db4109bf6b
Daniela Pino <dpinoc00@estudiantes.unileon.es>
2026-05-29 14:22:00 +0200

    Refactor LoginVO to use public attributes

--- src/modelo/vo/LoginVO.py
+++ src/modelo/vo/LoginVO.py
@@ -1,12 +1,4 @@
 class LoginVO:
     def __init__(self, email, password_hash):
-        self.__email = email
-        self.__password_hash = password_hash
-
-    @property
-    def email(self):
-        return self.__email
-
-    @property
-    def password_hash(self):
-        return self.__password_hash
+        self.email         = email
+        self.password_hash = password_hash
